e05464b7cef523d5f2ed4e886c6b9edb7f61c81f
Rodrigo Rael <rodrigorael53@gmail.com>
2021-04-09 00:55:44 -0300

    creating the CRUD of the products route and testing the routes

--- src/controllers/product/product.controller.ts
+++ src/controllers/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from 'src/models/product.model';
 import { Repository } from 'typeorm';
@@ -9,9 +18,37 @@ export class ProductController {
     @InjectRepository(Product) private productRepo: Repository<Product>,
   ) {}
 
+  @Get()
+  index() {
+    return this.productRepo.find();
+  }
+
+  @Get(':id')
+  show(@Param('id') id: string) {
+    return this.productRepo.findOneOrFail(id);
+  }
+
   @Post()
   store(@Body() body) {
     const product = this.productRepo.create(body);
     return this.productRepo.save(product);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body) {
+    await this.productRepo.findOneOrFail(id);
+    // dentro do productRepo.update eu passo 2 parâmetros, o primeiro é o
+    // critério que ele vai buscar para fazer a atualização eu eu passo o body
+    // como segundo parâmetro com os dados que vão ser alterados
+    this.productRepo.update({ id: +id }, body);
+
+    return await this.productRepo.findOne(id);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id') id: string) {
+    await this.productRepo.findOneOrFail(id);
+    this.productRepo.delete(id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ModelNotFoundExceptionFilter } from './exception-filters/model-not-found.exception-filter';
 
 async function bootstrap() {
   // está criando uma instância da aplicação a partir da classe AppModule
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ModelNotFoundExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
